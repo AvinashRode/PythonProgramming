2da2680d305e4a4e61f450e0735ed775ace4708d
Avinash <rode.avinash007@gmail.com>
2020-04-09 18:32:52 +0530

    loops comments

--- untitled0.py
+++ untitled0.py
@@ -69,4 +69,12 @@ for variable in range(20):
         
 for letter in 'AVINASH':
     print(letter)
-          
+    
+    '''
+    
+for variable in range(20):
+    if variable % 4 == 0:
+        print(variable)
+    if variable % 16 == 0:
+        print('Foo!')
+        '''
